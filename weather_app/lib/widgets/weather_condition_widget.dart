66984dee26301a075c47e1eec0f0edcd252adfc1
Alex Liang <126375355+alux444@users.noreply.github.com>
2025-10-01 09:59:37 +1300

    🎨 style(weather app): widget updates

--- weather_app/lib/widgets/weather_condition_widget.dart
+++ weather_app/lib/widgets/weather_condition_widget.dart
@@ -11,75 +11,82 @@ class WeatherConditionWidget extends StatelessWidget {
     return Card(
       elevation: 4.0,
       margin: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Text(
-            weatherData.cityName,
-            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-              fontWeight: FontWeight.bold,
-            ), // gets the headlineMedium theme and then adds bold font weight
-          ),
-          Text(
-            weatherData.country,
-            style: Theme.of(
-              context,
-            ).textTheme.bodyLarge?.copyWith(color: Colors.grey[500]),
-          ),
-          const SizedBox(height: 24),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              weatherData.cityName,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+              ), // gets the headlineMedium theme and then adds bold font weight
+            ),
+            Text(
+              weatherData.country,
+              style: Theme.of(
+                context,
+              ).textTheme.bodyLarge?.copyWith(color: Colors.grey[500]),
+            ),
+            const SizedBox(height: 24),
 
-          // weather icon + temp
-          Row(
-            children: [
-              Image.network(
-                weatherData.iconUrl,
-                width: 100,
-                height: 100,
-                errorBuilder: (context, error, stackTrace) {
-                  return const Icon(Icons.cloud, size: 100, color: Colors.grey);
-                },
-              ),
-              const SizedBox(width: 8),
-              Column(
-                children: [
-                  Text(
-                    '${weatherData.temperatureC.round()}°C',
-                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
+            // weather icon + temp
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  weatherData.iconUrl,
+                  width: 100,
+                  height: 100,
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(
+                      Icons.cloud,
+                      size: 100,
+                      color: Colors.grey,
+                    );
+                  },
+                ),
+                const SizedBox(width: 8),
+                Column(
+                  children: [
+                    Text(
+                      '${weatherData.temperatureC.round()}°C',
+                      style: Theme.of(context).textTheme.headlineLarge
+                          ?.copyWith(fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  Text(
-                    '${weatherData.temperatureF.round()}°F',
-                    style: Theme.of(
-                      context,
-                    ).textTheme.bodyLarge?.copyWith(color: Colors.grey[500]),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 14),
+                    Text(
+                      '${weatherData.temperatureF.round()}°F',
+                      style: Theme.of(
+                        context,
+                      ).textTheme.bodyLarge?.copyWith(color: Colors.grey[500]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 14),
 
-          // weather condition
-          Text(
-            weatherData.condition,
-            style: Theme.of(
-              context,
-            ).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.w500),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 14),
+            // weather condition
+            Text(
+              weatherData.condition,
+              style: Theme.of(
+                context,
+              ).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.w500),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 14),
 
-          // weather details
-          Container(),
-          const SizedBox(height: 14),
+            // weather details
+            Container(),
+            const SizedBox(height: 14),
 
-          Text(
-            'Last updated: ${weatherData.lastUpdated}',
-            style: Theme.of(
-              context,
-            ).textTheme.bodySmall?.copyWith(color: Colors.grey[500]),
-          ),
-        ],
+            Text(
+              'Last updated: ${weatherData.lastUpdated}',
+              style: Theme.of(
+                context,
+              ).textTheme.bodySmall?.copyWith(color: Colors.grey[500]),
+            ),
+          ],
+        ),
       ),
     );
   }
